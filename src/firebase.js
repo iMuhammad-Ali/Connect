d1e4e7a1609c88b430cb1d3ab367c76c59e25359
Muhammad Ali <105636293+iMuhammad-Ali@users.noreply.github.com>
2023-08-15 10:08:26 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth"
-import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBBhn7oVYceBRrHdmfckTWWulgGtVNAu0A",
-    authDomain: "connect-me-360.firebaseapp.com",
-    databaseURL: "https://connect-me-360-default-rtdb.firebaseio.com",
-    projectId: "connect-me-360",
-    storageBucket: "connect-me-360.appspot.com",
-    messagingSenderId: "183998015918",
-    appId: "1:183998015918:web:c20421dbf01e41cd4a803f",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const database = getDatabase(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
